d1944a3e6ecd02198a33d990472024ca02b8ce5a
Mesut-q <117123130+Mesut-q@users.noreply.github.com>
2023-10-03 19:25:44 +0300

    Update ödev 2.js

--- ödev 2.js
+++ ödev 2.js
@@ -74,7 +74,81 @@ app.delete('/todo-list/:id', async (req, res) => {
   }
 });
 
+
+
 // Sunucuyu dinlemeye başlamak
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+
+
+//Birim testler
+npm install --save-dev jest supertest mongoose
+
+const request = require('supertest');
+const mongoose = require('mongoose');
+const app = require('../app'); // Express uygulamanız
+const Todo = require('../models/todoModel');
+
+beforeAll(async () => {
+  await mongoose.connect('mongodb://localhost:27017/testdb', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
+
+afterAll(async () => {
+  
+  await mongoose.connection.close();
+});
+
+describe('Todo CRUD işlemleri', () => {
+  // Bir test Todo nesnesini tanımlarız
+  const testTodo = {
+    name: 'Test Todo',
+    status: 'Backlog',
+  };
+
+  let oluşturulanTodo;
+
+  it('Yeni bir Todo oluşturmalı', async () => {
+    const yanıt = await request(app)
+      .post('/api/todos')
+      .send(testTodo)
+      .expect(201);
+
+    oluşturulanTodo = yanıt.body;
+    expect(oluşturulanTodo).toHaveProperty('_id');
+    expect(oluşturulanTodo.name).toBe(testTodo.name);
+    expect(oluşturulanTodo.status).toBe(testTodo.status);
+  });
+
+  it('Todo listesini almalı', async () => {
+    const yanıt = await request(app)
+      .get('/api/todos')
+      .expect(200);
+
+    const todos = yanıt.body;
+    expect(Array.isArray(todos)).toBe(true);
+  });
+
+  it('Todo güncellenmeli', async () => {
+    const güncellenmişTodo = { ...oluşturulanTodo, name: 'Güncellenmiş Todo' };
+    const yanıt = await request(app)
+      .put(`/api/todos/${oluşturulanTodo._id}`)
+      .send(güncellenmişTodo)
+      .expect(200);
+
+    expect(yanıt.body.name).toBe(güncellenmişTodo.name);
+  });
+
+  it('Todo silinmeli', async () => {
+    await request(app)
+      .delete(`/api/todos/${oluşturulanTodo._id}`)
+      .expect(204);
+
+    const silinenTodo = await Todo.findById(oluşturulanTodo._id);
+    expect(silinenTodo).toBeNull();
+  });
 });
